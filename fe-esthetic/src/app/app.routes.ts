f726e1b5125cf5b8efbcbedac3801f01f6093429
momo <momo@MacBook-Pro-de-momo.local>
2024-02-09 14:55:13 -0500

    homepage without all styling

--- fe-esthetic/src/app/app.routes.ts
+++ fe-esthetic/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ClassesComponent } from './pages/classes/classes.component';
 
 export const routes: Routes = [
                                 { path: '', component: HomeComponent },
+                                { path: 'classes', component: ClassesComponent },
                                 ];
