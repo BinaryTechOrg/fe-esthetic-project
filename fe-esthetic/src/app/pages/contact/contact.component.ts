1d2f26c4f5415ca338f836c30cfc2a140c5cad9c
momo <momo@MacBook-Pro-de-momo.local>
2024-02-17 14:53:20 -0500

    test publish

--- fe-esthetic/src/app/pages/contact/contact.component.ts
+++ fe-esthetic/src/app/pages/contact/contact.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderComponent } from '../../common/header/header.component';
 import { FooterComponent } from '../../common/footer/footer.component';
+import { UnderHeaderComponent } from '../../common/under-header/under-header.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, HeaderComponent, FooterComponent],
+  imports: [ReactiveFormsModule, HeaderComponent, FooterComponent, UnderHeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
